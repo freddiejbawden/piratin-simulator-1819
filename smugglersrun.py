c523961fea1631f27787d1649d4e9afeccc27071
Stewart Wilson <stewart.wilson98@gmail.com>
2018-11-17 21:27:56 +0000

    We're in the money

--- smugglersrun.py
+++ smugglersrun.py
@@ -1,7 +1,10 @@
 import math
 import pygame
 import random
-from os import path
+import os
+from pygame.math import Vector2
+from math import tan, radians, degrees, copysign
+
 
 
 ####### COLOURS ########
@@ -20,92 +23,45 @@ MAX_SPEED = 5
 ########################
 
 
-## assets folder
-img_dir = path.join(path.dirname(__file__), 'sprites')
+############# Sprites #############
+all_sprites = pygame.sprite.Group()
+blocks = pygame.sprite.Group()
+cannon_list = pygame.sprite.Group()
+###################################
 
 
-# class Player(pygame.sprite.Sprite):
-#
-#     def __init__(self):
-#         """ Set up the player on creation. """
-#         # Call the parent class (Sprite) constructor
-#         super().__init__()
-#
-#         self.image = pygame.Surface([20, 20])
-#         self.image.fill(RED)
-#
-#         self.rect = self.image.get_rect()
-#
-#
-#
 
-class Player(pygame.sprite.Sprite):
+class Player:
     def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.imageMaster = pygame.image.load(path.join(img_dir, 'player_boat.png'))
-        self.imageMaster = self.imageMaster.convert()
-        self.image = pygame.transform.scale(self.imageMaster, (75, 75))
-        self.original_image = self.image
-
-        self.position = vec(WIDTH / 2, HEIGHT / 2)
-        self.rect = self.image.get_rect(center=self.position)
-        
         self.anchored = False
-
+        self.position = Vector2(200,200)
         self.velocity = Vector2(0.0, 0.0)
         self.angle = 0.0
-        self.length = 4
-        self.max_acceleration = 3
-        self.max_steering = 6
-        self.max_velocity = 20
+        self.length = 16
+        self.max_acceleration = 5
+        self.max_steering = 15
+        self.max_velocity = 50
         self.brake_deceleration = 10
         self.free_deceleration = 2
 
+        self.acceleration = 0.0
+        self.steering = 0.0
 
+    def update(self, dt):
+        self.velocity -= (0,self.acceleration * 0.2)
+        self.velocity.y = max(-self.max_velocity, min(self.velocity.y, self.max_velocity))
 
-    def update(self):
-        if(not self.anchored ):
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_LEFT]:
-                #if(self.vel.length()>0.0):
-                    #self.vel -= self.acceleration
-                self.angle_speed = -1
-                player.rotate()
-            if keys[pygame.K_RIGHT]:
-                #if(self.vel.length()>0.0):
-                    #self.vel -= self.acceleration
-                self.angle_speed = 1
-                player.rotate()
-            # If up or down is pressed, accelerate the ship by
-            # adding the acceleration to the velocity vector.
-            if keys[pygame.K_UP]:
-                self.vel += self.acceleration
-            if keys[pygame.K_DOWN]:
-                if(self.vel[1]<0):
-                    self.vel -= self.acceleration
+        if self.steering:
+            turning_radius = self.length / tan(radians(self.steering))
+            angular_velocity = self.velocity.y / turning_radius
+        else:
+            angular_velocity = 0
 
-            # max speed
-            if self.vel.length() > MAX_SPEED:
-                self.vel.scale_to_length(MAX_SPEED)
+        self.position += self.velocity.rotate(-self.angle) * dt
+        self.angle += degrees(angular_velocity) * dt
 
-            self.position += self.vel
-            self.rect.center = self.position
     def anchor(self):
         self.anchored = True
-        self.change_y = 0
-
-
-
-    def rotate(self):
-        # Rotate the acceleration vector.
-        self.acceleration.rotate_ip(self.angle_speed)
-        self.angle += self.angle_speed
-        if self.angle > 360:
-            self.angle -= 360
-        elif self.angle < 0:
-            self.angle += 360
-        self.image = pygame.transform.rotate(self.original_image, -self.angle)
-        self.rect = self.image.get_rect(center=self.rect.center)
 
     def wrap_around_screen(self):
         """Wrap around screen."""
@@ -120,133 +76,128 @@ class Player(pygame.sprite.Sprite):
 
 
 
+class Cannonball(pygame.sprite.Sprite):
+    """ This class represents the cannonball . """
+    def __init__(self, x, y, side):
+        # Call the parent class (Sprite) constructor
+        super().__init__()
+        self.image = pygame.Surface([4, 10])
+        self.image.fill(BLACK)
+        self.rect = self.image.get_rect()
+        self.position = Vector2(x,y)
+        self.side = side
+
+
+    def update(self):
+        """ Move the bullet. """
+        if(self.side == 1):
+            self.position.x -= 3
+        else:
+            self.position.x += 3
 
 
 
+class Game:
+    def __init__(self):
 
-######## initialize pygame and create window ##########
-pygame.init()
-pygame.mixer.init()  ## For sound
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Smugglers Run")
-clock = pygame.time.Clock()     ## For syncing the FPS
-#######################################################
+        pygame.init()
+        pygame.mixer.init()  ## For sound
+        pygame.display.set_caption("Smugglers Run")
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.ticks = 60
+        self.finished = False
+
+    def run(self):
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        image_path = os.path.join(current_dir, "player_boat.png")
+        player_image = pygame.image.load(image_path)
+        player_image = pygame.transform.scale(player_image, (100, 100))
+        player = Player()
+        #all_sprites.add(player)
+        ppu = 2
+
+        while not self.finished:
+            dt = self.clock.get_time() / 1000
+
+            # Event queue
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.exit = True
+
+            # User input
+            pressed = pygame.key.get_pressed()
+
+            if pressed[pygame.K_UP]:
+                if player.velocity.y < 0:
+                    player.acceleration = player.brake_deceleration
+                else:
+                    player.acceleration += 1 * dt
+            elif pressed[pygame.K_DOWN]:
+                if player.velocity.y > 0:
+                    player.acceleration = -player.brake_deceleration
+                else:
+                    player.acceleration -= 1 * dt
+            elif pressed[pygame.K_SPACE]:
+                if abs(player.velocity.y) > dt * player.brake_deceleration:
+                    player.acceleration = copysign(player.brake_deceleration, player.velocity.y)
+                else:
+                    player.acceleration = -player.velocity.x / dt
+            else:
+                if abs(player.velocity.y) > dt * player.free_deceleration:
+                    player.acceleration = copysign(player.free_deceleration, player.velocity.y)
+                else:
+                    if dt != 0:
+                        player.acceleration = -player.velocity.y / dt
+            player.acceleration = max(-player.max_acceleration, min(player.acceleration, player.max_acceleration))
+
+            if pressed[pygame.K_RIGHT]:
+                player.steering += 10 * dt
+            elif pressed[pygame.K_LEFT]:
+                player.steering -= 10 * dt
+            else:
+                player.steering = 0
+            player.steering = max(-player.max_steering, min(player.steering, player.max_steering))
+
+            if pressed[pygame.K_1]:
+                # Fire a bullet if the user clicks the mouse button
+                cannonball = Cannonball(player.position.x, player.position.y, 1)
+                print("test")
+                # Set the bullet so it is where the player is
+
+                # Add the bullet to the lists
+                all_sprites.add(cannonball)
+                cannon_list.add(cannonball)
+            elif pressed[pygame.K_2]:
+                # Fire a bullet if the user clicks the mouse button
+                cannonball = Cannonball(player.position.x, player.position.y, 0)
+                # Set the bullet so it is where the player is
+
+                # Add the bullet to the lists
+                all_sprites.add(cannonball)
+                cannon_list.add(cannonball)
+
+            for cannonball in cannon_list:
+
+                # Remove the bullet if it flies up off the screen
+                if cannonball.rect.y < -10:
+                    cannon_list.remove(bullet)
+                    all_sprites.remove(bullet)
+
+            # Logic
+            player.update(dt)
+
+            # Drawing
+            self.screen.fill(WHITE)
+            rotated = pygame.transform.rotate(player_image, player.angle)
+
+            rect = rotated.get_rect(center=player.position)
+            self.screen.blit(rotated, player.position * ppu - (rect.width / 2, rect.height / 2))
+            pygame.display.flip()
+            self.clock.tick(self.ticks)
+        pygame.quit()
 
 
-
-############# Sprites #############
-all_sprites = pygame.sprite.Group()
-blocks = pygame.sprite.Group()
-bullets = pygame.sprite.Group()
-###################################
-
-
-# Create a red player block
-player = Player()
-all_sprites.add(player)
-
-
-######### MAIN GAME LOOP #########
-done = False
-
-
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-        # if event.type == pygame.KEYDOWN:
-        #         if event.key == pygame.K_LEFT:
-        #             player.turnLeft()
-        #         if event.key == pygame.K_RIGHT:
-        #             player.turnRight()
-        #         if event.key == pygame.K_UP:
-        #             player.speedUp()
-        #         if event.key == pygame.K_SPACE:
-        #             player.anchor()
-
-
-    player.wrap_around_screen()
-    all_sprites.update()
-
-
-    # --- Draw a frame --- #
-
-    # Clear the screen
-    screen.fill(WHITE)
-
-    # Draw all the spites
-    all_sprites.draw(screen)
-
-    # Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
-
-    pygame.display.set_caption('angle {:.1f} accel {} accel angle {:.1f}, velocity {}'.format(
-        player.angle, player.acceleration, player.acceleration.as_polar()[1], player.vel))
-    pygame.display.update()
-    # --- Limit to 20 frames per second
-    clock.tick(60)
-
-pygame.quit()
-
-
-
-
-
-
-
-
-
-################# MEGA COMMENT SHIT THAT WILL BE USEFUL SOON ###################
-
-# class Bullet(pygame.sprite.Sprite):
-#     """ This class represents the bullet . """
-#     def __init__(self):
-#         # Call the parent class (Sprite) constructor
-#         super().__init__()
-#
-#         self.image = pygame.Surface([4, 10])
-#         self.image.fill(BLACK)
-#
-#         self.rect = self.image.get_rect()
-#
-#     def update(self):
-#         """ Move the bullet. """
-#         self.rect.y -= 3
-
-# class Block(pygame.sprite.Sprite):
-#     """ This class represents the block. """
-#     def __init__(self, color):
-#         # Call the parent class (Sprite) constructor
-#         super().__init__()
-#
-#         self.image = pygame.Surface([20, 15])
-#         self.image.fill(color)
-#
-#         self.rect = self.image.get_rect()
-
-# for bullet in bullets:
-#
-#     block_hit_list = pygame.sprite.spritecollide(bullet, blocks, True)
-#
-#     for block in block_hit_list:
-#         bullets.remove(bullet)
-#         all_sprites.remove(bullet)
-#
-#     # Remove bullets off screen
-#     if bullet.rect.y < -10:
-#         bullets.remove(bullet)
-#         all_sprites.remove(bullet)
-
-        #
-        #
-        # if event.type == pygame.KEYUP:
-        #     if event.key == pygame.K_LEFT and player.change_x < 0:
-        #         player.stop()
-        #     if event.key == pygame.K_RIGHT and player.change_x > 0:
-        #         player.stop()
-        #     if event.key == pygame.K_DOWN and player.change_y > 0:
-        #         player.stop()
-        #     if event.key == pygame.K_UP and player.change_y < 0:
-        #         player.stop()
+if __name__ == '__main__':
+    game = Game()
+    game.run()
