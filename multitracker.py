b0893561f68f307c594a4aed25fc8e575990bd82
hhowley <hjhowley99@hotmail.co.uk>
2018-11-18 09:38:36 +0000

    set sail mateys

---
+++ multitracker.py
@@ -0,0 +1,109 @@
+import cv2 as cv
+import math
+import numpy as np
+
+
+class Target:
+	def __init__(self):
+		self.capture = cv.VideoCapture(0)
+
+
+
+
+	def run(self):
+		#initiate font
+		font = cv.FONT_HERSHEY_SIMPLEX
+
+		frame_height = int(self.capture.get(cv.CAP_PROP_FRAME_HEIGHT))
+		frame_width = int(self.capture.get(cv.CAP_PROP_FRAME_WIDTH))
+
+		#instantiate images
+		#hsv_img=cv.CreateImage(cv.GetSize(cv.QueryFrame(self.capture)),8,3)
+		hsv_img = np.zeros((frame_height, frame_width, 3), dtype=np.uint8)
+		threshold_img1 = np.zeros((frame_height, frame_width, 1), dtype=np.uint8)
+		threshold_img1a = np.zeros((frame_height, frame_width, 1), dtype=np.uint8)
+		threshold_img2 = np.zeros((frame_height, frame_width, 1), dtype=np.uint8)
+		i=0
+
+
+
+
+		#capture the image from the cam
+		ret, img=self.capture.read()
+
+		#convert the image to HSV
+		hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+
+
+		#threshold the image to isolate two colors
+		cv.inRange(hsv_img,(164,102,187),(218,222,255),threshold_img2) #pink
+		cv.inRange(hsv_img,(80,182,136),(110,255,255),threshold_img1)  #blue
+		#cv.inRange(hsv_img,(14,159,77),(38,234,192),threshold_img1)
+
+
+		#determine the moments of the two objects
+		#threshold_img1=cv.getMat(threshold_img1)
+		#threshold_img2=cv.getMat(threshold_img2)
+		moments1=cv.moments(threshold_img1)
+		moments2=cv.moments(threshold_img2)
+		area1 = moments1['m00']
+		area2 = moments2['m00']
+
+		#initialize x and y
+		x1,y1,x2,y2=(1,2,3,4)
+		coord_list=[x1,y1,x2,y2]
+		for x in coord_list:
+			x=0
+		#there can be noise in the video so ignore objects with small areas
+		if (area1 >200000):
+			#x and y coordinates of the center of the object is found by dividing the 1,0 and 0,1 moments by the area
+			x1=int(moments1['m10']/area1)
+			y1=int(moments1['m01']/area1)
+			print [x1,y1]
+			#draw circle
+			cv.circle(img,(x1,y1),2,(0,255,0),20)
+
+			#write x and y position
+			cv.putText(img,str(x1)+","+str(y1),(x1,y1+20),font, 1,(255,255,255)) #Draw the text
+
+		if (area2 >100000):
+			#x and y coordinates of the center of the object is found by dividing the 1,0 and 0,1 moments by the area
+			x2=int(moments2['m10']/area2)
+			y2=int(moments2['m01']/area2)
+			print [x2,y2]
+			#draw circle
+			cv.circle(img,(x2,y2),2,(255,0,0),20)
+
+			cv.putText(img,str(x2)+","+str(y2),(x2,y2+20),font, 1,(255,255,255)) #Draw the text
+			cv.line(img,(x1,y1),(x2,y2),(0,255,0),4,cv.LINE_AA)
+			#draw line and angle
+			cv.line(img,(x1,y1),(frame_height, frame_width),(100,100,100,100),4,cv.LINE_AA)
+		x1=float(x1)
+		y1=float(y1)
+		x2=float(x2)
+		y2=float(y2)
+		try:
+			angle = int(math.atan((y1-y2)/(x2-x1))*180/math.pi)
+			print(angle)
+			if angle > 0:
+				return angle +270
+			else:
+				return 90-math.fabs(angle)
+			cv.putText(img,str(angle),(int(x1)+50,(int(y2)+int(y1))/2),font, 4,(255,255,255))
+		except ZeroDivisionError:
+			print("lost contact!")
+			return 0
+
+			#cv.writeFrame(writer,img)
+
+
+			#display frames to users
+			#cv.imshow("Threshold1",threshold_img1)
+			#cv.imshow("Threshold2",threshold_img2)
+			#cv.imshow("hsv",hsv_img)
+			# Listen for ESC or ENTER key
+
+
+if __name__=="__main__":
+	t = Target()
+	t.run()
--- smugglersrun.py
+++ smugglersrun.py
@@ -4,6 +4,7 @@ import random
 import os
 import numpy as np
 from pygame.math import Vector2
+from multitracker import Target
 from math import tan, radians, degrees, copysign
 
 
@@ -18,8 +19,8 @@ BLUE  = (  0,  0,  255)
 
 
 ####### CONSTANTS ######
-WIDTH = 800
-HEIGHT = 600
+WIDTH = 1920
+HEIGHT = 1080
 MAX_SPEED = 5
 ########################
 
@@ -30,23 +31,6 @@ blocks = pygame.sprite.Group()
 cannon_list = pygame.sprite.Group()
 ###################################
 
-class Bullet(pygame.sprite.Sprite):
-    """ This class represents the bullet . """
-    def __init__(self):
-        # Call the parent class (Sprite) constructor
-        super().__init__()
-
-        self.image = pygame.Surface([4, 10])
-        self.image.fill(BLACK)
-
-        self.position = Vector2(200,200)
-        self.velocity = Vector2(0.0, 0.0)
-        self.angle = 90.0
-        self.acceleration = 1.2
-    def update(self):
-        """ Move the bullet. """
-        self.velocity -= (self.acceleration * 0.2, 0.0)
-        self.velocity.x = 1.2
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -57,20 +41,20 @@ class Player(pygame.sprite.Sprite):
         self.position = Vector2(200,200)
         self.velocity = Vector2(0.0, 0.0)
         self.angle = 0.0
-        self.length = 16
-        self.max_acceleration = 5
-        self.max_steering = 15
-        self.max_velocity = 50
+        self.length = 14
+        self.max_acceleration = 7
+        self.max_steering = 25
+        self.max_velocity = 70
         self.brake_deceleration = 10
-        self.free_deceleration = 2
-
+        self.free_deceleration = 0
+        self.angle_getter = Target()
 
 
         self.acceleration = 0.0
         self.steering = 0.0
 
     def update(self, dt):
-        self.velocity -= (0,self.acceleration * 0.2)
+        self.velocity -= (0,self.acceleration * 0.5)
         self.velocity.y = max(-self.max_velocity, min(self.velocity.y, self.max_velocity))
 
         if self.steering:
@@ -116,7 +100,8 @@ class Game:
         ppu = 2
 
         while not self.finished:
-            dt = self.clock.get_time() / 1000
+            dt = 0.017
+
 
             # Event queue
             for event in pygame.event.get():
@@ -127,6 +112,7 @@ class Game:
             pressed = pygame.key.get_pressed()
 
             if pressed[pygame.K_UP]:
+                print("UP")
                 if player.velocity.y < 0:
                     player.acceleration = player.brake_deceleration
                 else:
@@ -149,33 +135,19 @@ class Game:
                         player.acceleration = -player.velocity.y / dt
             player.acceleration = max(-player.max_acceleration, min(player.acceleration, player.max_acceleration))
 
-            if pressed[pygame.K_RIGHT]:
+
+            a = player.angle_getter.run()
+            if a <= 90 and a >= 10:
+                #if pressed[pygame.K_RIGHT]:
                 player.steering += 10 * dt
-            elif pressed[pygame.K_LEFT]:
+            elif a <= 350 and a >= 270:
                 player.steering -= 10 * dt
             else:
                 player.steering = 0
             player.steering = max(-player.max_steering, min(player.steering, player.max_steering))
 
-            if pressed[pygame.K_f]:
-                # Fire a bullet if the user clicks the mouse button
-                bullet = Bullet()
-                # Set the bullet so it is where the player is
-
-                # Add the bullet to the lists
-                all_sprites.add(bullet)
-                cannon_list.add(bullet)
-                rotated_bullet = pygame.transform.rotate(bullet.image, bullet.angle)
-                rect_bullet = rotated_bullet.get_rect(center=bullet.position)
-                self.screen.blit(rotated_bullet, bullet.position * ppu - (rect.width / 2, rect.height / 2))
-
-
-
             # Game Logic
             player.update(dt)
-            cannon_list.update()
-            #print(cannon_list)
-            #print("Player Position: {}, Player Velocity: {}, Player acceleration: {}, Player center: {}, Player angle: {}." .format(player.position, player.velocity, player.acceleration, player.rect, player.angle,))
 
             # Drawing
             self.screen.fill(WHITE)
@@ -183,10 +155,10 @@ class Game:
 
 
             rect = rotated.get_rect(center=player.position)
-            print(pygame.mouse.get_pos())
 
 
             self.screen.blit(rotated, player.position * ppu - (rect.width / 2, rect.height / 2))
+            print("ANGLE {}. velocity {}, acceleration {}." .format(player.angle, player.velocity, player.acceleration))
 
             pygame.display.flip()
             self.clock.tick(self.ticks)
